77885d4577004255cf078af4d48879b54c455de9
Santeri Paavolainen <santeri.paavolainen@codento.com>
2012-04-17 14:06:23 +0300

    Moved stuff out of code file to separate todo file.

---
+++ TODO.rst
@@ -0,0 +1,29 @@
+============
+Things To Do
+============
+
+* BETTER DOCUMENTATION (and somewhere else than in this source)
+
+* Actually put some documentation in the code too
+
+* Maybe promote "grant" as first-order entity, now the required
+  information is passed in an ugly manner via tuples
+
+* Add a configuration file that can add/remove can default to
+  (automatically close and plug holes)
+
+* Perhaps some --background option to allow this run as a background
+  program and periodically update entries.. so that you could even run
+  this as a session too. hmm, maybe not, that's not terribly good
+  security practice
+
+* Change logic so that its possible to defined different port ranges
+  to different sgs (maybe only for config file use)
+
+* Implement --initialize option
+
+* Move repository to real project, make easily packaged and built
+  script (can-opener, not can-opener.py)
+
+* Add license information (GPL or some similar suitable for a
+  command-line tool)
--- can-opener.py
+++ can-opener.py
@@ -25,25 +25,6 @@
 # "holes" in a firewall with regards to their current public IP
 # address. These holes can then be also automatically "removed" when
 # their validity lifetime is exceeded.
-#
-
-# TODO LIST:
-#
-# * BETTER DOCUMENTATION (and somewhere else than in this source)
-# * actually put some documentation in the code too
-# * maybe promote "grant" as first-order entity, now the required
-#   information is passed in an ugly manner via tuples
-# * add a configuration file that can add/remove can default to
-#   (automatically close and plug holes)
-# * perhaps some --background option to allow this run as a background
-#   program and periodically update entries.. so that you could even
-#   run this as a session too. hmm, maybe not, that's not terribly
-#   good security practice
-# * change logic so that its possible to defined different port ranges
-#   to different sgs (maybe only for config file use)
-# * implement --initialize option
-# * move repository to real project, make easily packaged and built
-#   script (can-opener, not can-opener.py)
 
 from boto.exception import SDBResponseError, BotoServerError
 import boto.ec2
