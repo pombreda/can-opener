694e48d91c9c3a09331fa7662cba6e49d7b82446
Santeri Paavolainen <santeri.paavolainen@codento.com>
2012-11-15 10:20:59 +0200

    Added GPL notice to the source code.

--- can-opener.py
+++ can-opener.py
@@ -3,6 +3,19 @@
 # (c) Codento Oy, Helsinki Finland, 2012
 # Authors: Santeri Paavolainen, <santeri.paavolainen@codento.com>
 #
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # Tool to manage EC2 security group automatically and *STATEFULLY*,
 # granting accesses to given (or automatically determined) ip
 # addresses.
